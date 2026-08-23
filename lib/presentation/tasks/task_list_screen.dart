a4ee84e9aa84a0c5b0ad0f6fa42a457472a2acfb
Your Name <vaibhavjoshi0709@gmail.com>
2026-08-23 23:07:19 +0530

    refactor: move task_card widget to core/widgets

--- lib/presentation/tasks/task_list_screen.dart
+++ lib/presentation/tasks/task_list_screen.dart
@@ -8,7 +8,7 @@ import 'package:taskflow/presentation/tasks/task_form_dialog.dart';
 import 'package:taskflow/core/widgets/custom_card.dart';
 import 'package:taskflow/core/widgets/custom_app_bar.dart';
 import 'package:taskflow/core/widgets/custom_button.dart';
-import 'package:taskflow/presentation/widgets/task_card.dart';
+import 'package:taskflow/core/widgets/task_card.dart';
 import 'package:taskflow/core/widgets/list_shimmer.dart';
 import 'package:taskflow/core/constants/task_constants.dart';
 
